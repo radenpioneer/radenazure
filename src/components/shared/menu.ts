9f7d2118778e56f164c67182ed52606cb13c74b5
R. Ilham Sastronegoro <7355835+radenpioneer@users.noreply.github.com>
2022-06-07 09:15:27 +0000

    implement drawer

---
+++ src/components/shared/menu.ts
@@ -0,0 +1,20 @@
+const menu = [
+    {
+        name: 'Home',
+        path: '/'
+    },
+    {
+        name: 'Profil',
+        path: '/profil'
+    },
+    {
+        name: 'Catatan',
+        path: '/catatan'
+    },
+    {
+        name: 'Buat Website',
+        path: 'https://sngr.dev'
+    }
+]
+
+export default menu
